6a3c383e6bbed8931ca0e96d47e79289901151a1
Mert can Ünek <mrtcnunk@gmail.com>
2023-04-12 23:26:00 +0300

    final commit
    I wrote simpson model using oop and classes that introduce simpson family.

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,26 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Mertcan Ünek on 12.04.2023.
+//
+
+import Foundation
+import UIKit
+
+class Simpson{
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    
+    init(simpsonName: String, simpsonJob: String, simpsonImage: UIImage) {
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+    }
+    
+    
+    
+}
